de4d801277ecdfb6408dd65ac6443c44340a2721
JennaKim <jenna_kim1@hotmail.com>
2018-05-03 19:02:19 +0900

    get API

--- src/App.js
+++ src/App.js
@@ -2,38 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-
-
-
-
 class App extends Component {
+// Render: componentWillMount() -> render() -> componentDidMount()
+// Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
 
-  state = {
-  }
+  state = {};
 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies: [ 
-          {
-            title: "Fight Club",
-            poster: "https://i.pinimg.com/736x/fd/5e/66/fd5e662dce1a3a8cd192a5952fa64f02--classic-poster-classic-movies-posters.jpg"
-          },
-          {
-            title: "Dunkirk",
-            poster: "http://2.citynews.stgy.ovh/~shared/images/cinema/locandine/dunkirk-gzsm.jpg"
-          },
-          {
-            title: "Baby Driver",
-            poster: "http://getthechance.wales/wp-content/uploads/2017/06/BD_INTL_FNL_QUAD-Large_1000x750p_thumbnail-1.jpg"
-          },
-          {
-            title: "Perfume",
-            poster: "http://smashinghub.com/wp-content/uploads/2012/06/Use-AIDA-Formula.jpg"
-          } 
-        ]
-      })
-    }, 5000)
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+    .then(potato => potato.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
   }
 
   _renderMovies = () => {
